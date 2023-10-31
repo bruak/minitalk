f740ce878fd69494a2d302ab0a3627d8642d665d
Burak Soykan <32242237+bruak@users.noreply.github.com>
2023-10-31 16:43:06 +0300

    Add files via upload

--- client.c
+++ client.c
@@ -6,101 +6,58 @@
 /*   By: bsoykan <bsoykan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 16:40:08 by bsoykan           #+#    #+#             */
-/*   Updated: 2023/10/23 15:52:26 by bsoykan          ###   ########.fr       */
+/*   Updated: 2023/10/27 16:35:17 by bsoykan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <signal.h>
 #include "libft/libft.h"
 
-
-
-int main(int ac,char **av){
-	
-	int i;
-	int pid;
-	int power;
-	char *str;
-
-	i = 0;
-	power = 128;
-	if (ac < 3){
+int	checker(int ac, int pi)
+{
+	if (ac < 3)
+	{
 		ft_putstr_fd("ErRor: arguments not enough!!!\n", 2);
-		return (-1);
+		return (0);
 	}
-	else if (ac > 3){
+	else if (ac > 3)
+	{
 		ft_putstr_fd("ErRor: arguments too much!!!\n", 2);
-		return (-1);
+		return (0);
 	}
+	else if (pi == -1)
+	{
+		ft_putstr_fd("Bro not do that ok?, this is not win u cant use f5\n", 2);
+		return (0);
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		pid;
+	int		power;
+	char	*str;
+
+	power = 128;
+	i = -1;
 	pid = ft_atoi(av[1]);
 	str = av[2];
-	while (str[i]){
-		while (power >= 1){
-			if (str[i] & power) //if içerisindeki koda göre gönderim yapar
+	if (!checker(ac, pid))
+		return (-1);
+	while (str[++i])
+	{
+		while (power >= 1)
+		{
+			if (str[i] & power)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
 			power /= 2;
-		usleep(100);
+			usleep(100);
 		}
 		power = 128;
-		i++;
 	}
 	return (0);
 }
-
-
-/*
-örnek: 
-ABC
-A = 65 = 0b01000001 
-B = 66 = 0b01000010
-C = 67 = 0b01000011
-ilk önce A ile başlanır. A 65 olduğu için 8 kere döner.
-A = 0b01000001
-ilk önce 128 ile karşılaştırma yapılır en soldaki bit 1 ise sigusr1 gönderilir.
-server tarafında alınan sinyal sigusr1 ise 1 ise letter değişkenine power 128 olur
-*/
-
-
-//image.png
-
-/*
-her bir karakterin binary karşılığı 8 bit olduğu için 8 kere döndürüyoruz.
-str[i] & power ile str[i] nin binary karşılığının power ile and işlemi yapıyoruz.
-eğer sonuç 0 ise 0, 1 ise 1 oluyor. 0 ise SIGUSR2, 1 ise SIGUSR1 gönderiyoruz.
-power 2 nin 8. kuvvetinden başlıyor ve her döngüde 2 nin bir eksiğine bölünüyor.
-yani 128, 64, 32, 16, 8, 4, 2, 1 şeklinde.
-örnek:{
-str = "A"
-
-str[0] = 'A' = 65 = 0b01000001
-power = 128 = 0b10000000
-power = 64 = 0b01000000
-power = 32 = 0b00100000
-power = 16 = 0b00010000
-power = 8 = 0b00001000
-power = 4 = 0b00000100
-power = 2 = 0b00000010
-power = 1 = 0b00000001
-str[0] & power = 0b01000001 & 0b10000000 = 0b00000000 = 0
-kill(pid, SIGUSR2);
-
-bu şekilde her bir bitin ne olduğu açığa çıkar ve bu açığa çıkan bitleri de 
-kill fonksiyonu ile göndeririz. server üzerinde de yeniden bit to byte yapılır.
-}
-┌─────────────┬──────────────┬───────────────┐
-│ str[i]      │ power        │ str[i] & power│
-├─────────────┼──────────────┼───────────────┤
-│   0b00000001│    0b10000000│    0b00000000 │
-│   0b00000010│    0b10000000│    0b00000000 │
-│   0b00000100│    0b10000000│    0b00000000 │
-│   0b00001000│    0b10000000│    0b00000000 │
-│   0b00010000│    0b10000000│    0b00000000 │
-│   0b00100000│    0b10000000│    0b00000000 │
-│   0b01000000│    0b10000000│    0b00000000 │
-│   0b10000000│    0b10000000│    0b10000000 │
-└────────────┴──────────────┴─────────────── ┘
-
-*/
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: bsoykan <bsoykan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 17:01:56 by bsoykan           #+#    #+#             */
-/*   Updated: 2023/10/23 15:01:24 by bsoykan          ###   ########.fr       */
+/*   Updated: 2023/10/27 17:00:36 by bsoykan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,55 +20,26 @@ void	handler(int signum)
 
 	if (!power)
 		power = 128;
-	if (signum == SIGUSR1) // eğer gelen sinyal SIGUSR1 ise 1 ile kaydırma yapar ve toplar.
-		letter += power; // 1 ile kaydırma yapar ve toplar.
-	power /= 2; // power = 64, 32, 16, 8, 4, 2, 1 şeklinde çıkartma yapar.
-	if (power == 0){ 
+	if (signum == SIGUSR1)
+		letter += power;
+	power /= 2;
+	if (power == 0)
+	{
 		ft_putchar_fd(letter, 1);
 		power = 128;
 		letter = 0;
 	}
+	usleep(100);
 }
 
 int	main(void)
 {
-    ft_putstr_fd("PID: ", 1);
-    ft_putnbr_fd(getpid(), 1);
-    ft_putstr_fd("\n", 1);
-    signal(SIGUSR1, handler); // her bir bit için program dinleme yapar her bir bit için çalışır ve handler fonksiyonuna gider.
-    signal(SIGUSR2, handler); // her bir bit için program dinleme yapar her bir bit için çalışır ve handler fonksiyonuna gider. //statik olmasının sebebi her bir bit için çalışmasıdır.
-    while (1)
-        pause();
-    return (0);
+	ft_putstr_fd("\nPID ---------------------------------------> ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
+	return (0);
 }
-
-/*
-`letter` değişkeni her bit için toplanır. İşte bir örnek:
-
-Varsayalım ki `client` tarafından gönderilen 8 bitlik binary bir karakter, "01000001" yani 'A' karakteridir.
-
-1. İşlem: 
-   - Başlangıçta `letter = 0`, `power = 128`.
-   - İlk bit (en sol bit) 0 olduğu için `signum` değeri `SIGUSR2` olur.
-   - Çünkü `signum` değeri `SIGUSR2`, `letter` değeri değişmez (0).
-   - `power` değeri 64 olur (`power /= 2`).
-
-2. İşlem: 
-   - İkinci bit 1 olduğu için `signum` değeri `SIGUSR1` olur.
-   - `letter` değeri 64 ile toplanır (`letter += power`), yani `letter = 64`.
-   - `power` değeri 32 olur (`power /= 2`).
-
-3. İşlem: 
-   - Üçüncü bit 0 olduğu için `signum` değeri `SIGUSR2` olur.
-   - `letter` değeri değişmez (64).
-   - `power` değeri 16 olur (`power /= 2`).
-
-Bu işlem devam eder ve tüm 8 bit işlendikten sonra `power` değeri 0 olur ve tam bir karakter (burada 'A') elde edilir. `letter` değişkeni, her bit için sırasıyla işlem yaparak karakterin değerini toplar.
-*/
-// client 1 karakter için 8 döngü boyunca 1 ve 0 gönderiyor. sonrasında gönderilen bu değerler serverda handler fonksiyonuna gidiyor. 
-//bu fonksiyona gelen 0 ve 1 değerleri letter denilen değişkende toplanarak ortaya bitlerden toplanan bir karakter ortaya çıkıyor
-//tüm bitler bittikten sonra power 0 olur. fakat letter 0 olmadığı için ilk karakteri yazdırır tekrardan döngü devam eder. 
-//son seviyede sinyaller hep 0 olacağı için letter de 0 olur ve fonskiyondan çıkılarak program sonlanır.
-
-
-
